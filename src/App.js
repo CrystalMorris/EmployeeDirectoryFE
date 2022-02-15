03f301ebfabc415379b14eb96ad6eba746a9cd8b
Crystal <l.seagypsy.l@gmail.com>
2022-02-15 15:56:41 -0600

    added dom routes for update and get byId

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, Routes}from 'react-router-dom'
 import ListEmpComp from './components/ListEmpComp';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
 import CreateEmpComp from './components/CreateEmpComp';
+import UpdateEmpComp from './components/UpdateEmpComp';
+import OneEmployee from './components/OneEmployee';
 
 
-function App() {
- 
+const App=()=> {
   return (
     <div>
-      <Router>
-       
-            <HeaderComponent />
-            
+      <Router>       
+            <HeaderComponent />            
               <div className="container">
                   <Routes>
                       <Route exact path ="/"  element= {<ListEmpComp />}></Route>
                       <Route  path ="/employees" element = {<ListEmpComp />}></Route>
                       <Route  path ="/add-employee" element= {<CreateEmpComp />}></Route>
+                      <Route  path="/update-employee/:id" element = {<UpdateEmpComp />}></Route>
+                      <Route  path  ="/employees/:id" element={<OneEmployee/>}></Route>
                   </Routes>
               </div>
             <FooterComponent />
-          
       </Router>
     </div>
   );
