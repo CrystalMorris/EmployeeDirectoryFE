48ad3549671d9e2307cbeca5d1e556daeea70d82
Crystal <l.seagypsy.l@gmail.com>
2022-02-16 01:55:05 -0600

    added call to UpdateEmpComp as well as general formating and clean up

--- src/components/OneEmployee.jsx
+++ src/components/OneEmployee.jsx
@@ -1,77 +1,62 @@
-import React, { Component } from 'react';
-import EmployeeService from '../services/EmployeeService';
+import React, { Component } from "react";
+import EmployeeService from "../services/EmployeeService";
+import UpdateEmpComp from "./UpdateEmpComp";
 
-import UpdateEmpButton from './SelectEmpButton';
-
-
-
-
+import UpdateEmpButton from "./SelectEmpButton";
 
 class OneEmployee extends Component {
-    constructor(props) {
-        super(props);
-        
-        this.state = {
-            id: '',
-            firstName: '',
-            lastName: '',
-            email: ''
-        } 
-       
-    }
+  constructor(props) {
+    super(props);
 
-    componentDidMount(){
-        const url = window.location.href;
-        const id = url.slice(32);
-       EmployeeService.findEmployeesById(id).then((res)=>{
-        this.setState(res.data);
-       console.log(this.state);
-       console.log(this.state.id)
+    this.state = {
+      id: "",
+      firstName: "",
+      lastName: "",
+      email: "",
+    };
+  }
+
+  componentDidMount() {
+    const url = window.location.href;
+    const id = url.slice(32);
+
+    EmployeeService.findEmployeesById(id).then((res) => {
+      this.setState(res.data);
     });
-    
-}
-    
- 
-    
+  }
 
-    render() {
-        return (
-            <div>
-                
-                <h2 className='text-center'>Employee</h2>
+  render() {
+       return (
+      <div>
+        <h2 className="text-center">Employee</h2>
 
-                <div className='row'>
-                    <table className='table table-striped tabled-bordered'>
-                        <thead>
-                            <tr>
-                                <th>Id</th>
-                                <th>First Name</th>
-                                <th>Last Name</th>
-                                <th>Email</th>
-                                <th> Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                         
-                                   <tr >                                      
-                                       <td>{this.state.id}</td>
-                                       <td>{this.state.firstName}</td>
-                                       <td>{this.state.lastName}</td> 
-                                       <td>{this.state.email}</td> 
-                                       
-                                   </tr>
-                               
-                            
-                        </tbody>
-                    </table>
-
-
-                </div>
-
-
-            </div>
-        );
-    }
+        <div className="row">
+          <table className="table table-striped tabled-bordered col-md-10">
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+                <th> Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{this.state.id}</td>
+                <td>{this.state.firstName}</td>
+                <td>{this.state.lastName}</td>
+                <td>{this.state.email}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div className="row">
+          <UpdateEmpComp emp={this.state} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default OneEmployee;
