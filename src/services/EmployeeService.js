3cb400c9bd8b3598345db806cfbdb5746e94460c
Crystal <l.seagypsy.l@gmail.com>
2022-02-16 01:52:27 -0600

    created a separate axio route for updating each field

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -1,33 +1,38 @@
 import axios from "axios";
 
-const EMP_API_BASE_URL = "http://localhost:8080/api/v1/employees"
-const EMP_API_CREATE_URL = "http://localhost:8080/api/v1/add-employee"
-const EMP_API_UPDATE_URL = "http://localhost:8080/api/v1/update-employee/"
+const EMP_API_BASE_URL = "http://localhost:8080/api/v1/employees";
+const EMP_API_CREATE_URL = "http://localhost:8080/api/v1/add-employee";
+const EMP_API_UPDATE_URL = "http://localhost:8080/api/v1/update-employee/";
 
+class EmployeeService {
+  getEmployees() {
+    // console.log(axios.get(EMP_API_BASE_URL));
+    return axios.get(EMP_API_BASE_URL);
+  }
 
-class EmployeeService{
+  saveEmployee(fName, lName, eml) {
+    console.log(fName + "," + lName + "," + eml);
+    axios.post(EMP_API_CREATE_URL, {
+      firstName: fName,
+      lastName: lName,
+      email: eml,
+    });
+  }
 
-    getEmployees(){
-        console.log(axios.get(EMP_API_BASE_URL));
-        return axios.get(EMP_API_BASE_URL);
-    }
-
-    saveEmployee(fName, lName, eml){
-        console.log(fName + ',' + lName + ',' + eml);
-        axios.post(EMP_API_CREATE_URL,{firstName: fName, lastName: lName, email: eml});
-    }
-
-    findEmployeesById(id){
-        console.log(axios.get(EMP_API_BASE_URL+"/" + id).data);
-        //.then(response => response.data);
-        //console.log(returnedData.id);
-          return  axios.get(EMP_API_BASE_URL+"/" + id);
-          
-     }
-    updateEmployee(id, fName, lName, eml){
-        console.log(fName + ',' + lName + ',' + eml);
-        axios.put(EMP_API_UPDATE_URL + id,{firstName: fName, lastName: lName, email: eml})
-    }
-
+  findEmployeesById(id) {
+    return axios.get(EMP_API_BASE_URL + "/" + id);
+  }
+  updateEmployeeEmail(id, eml) {
+    console.log(id + "," + eml);
+    axios.put(EMP_API_UPDATE_URL + id, { email: eml });
+  }
+  updateEmployeeFirstName(id, fName) {
+    console.log(id + "," + fName);
+    axios.put(EMP_API_UPDATE_URL + id, { firstName: fName });
+  }
+  updateEmployeeLastName(id, lName) {
+    console.log(id + "," + lName);
+    axios.put(EMP_API_UPDATE_URL + id, { lastName: lName });
+  }
 }
-export default new EmployeeService()
+export default new EmployeeService();
