3d7b919b6d17ea5a1195ba28f7a6e5c1a9b345d0
Crystal <l.seagypsy.l@gmail.com>
2022-04-05 11:51:20 -0500

    added slash to end of api url

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const EMP_API_BASE_URL = "https://sb-backend-emplist.herokuapp.com/api/v1/employees";
+const EMP_API_BASE_URL = "https://sb-backend-emplist.herokuapp.com/api/v1/employees/";
 const EMP_API_CREATE_URL = "https://sb-backend-emplist.herokuapp.com/api/v1/add-employee";
 const EMP_API_UPDATE_URL = "https://sb-backend-emplist.herokuapp.com/api/v1/update-employee/";
 const EMP_API_DELETE_URL = "https://sb-backend-emplist.herokuapp.com/api/v1/delete-employee/";
@@ -22,7 +22,7 @@ class EmployeeService {
   }
 
   findEmployeesById(id) {
-    return axios.get(EMP_API_BASE_URL +'/' +id);
+    return axios.get(EMP_API_BASE_URL  + id);
   }
   updateEmployeeEmail(id, eml) {
     console.log(id + "," + eml);
