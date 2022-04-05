007f017bb7ccc4a648f323fe0adb71ac1acdd415
Crystal <l.seagypsy.l@gmail.com>
2022-04-05 13:39:21 -0500

    changed the slice for url of delete function

--- src/components/DeleteEmpComp.js
+++ src/components/DeleteEmpComp.js
@@ -3,4 +3,4 @@ import EmployeeService from "../services/EmployeeService";
 import { useNavigate } from "react-router-dom";
 
 const url = window.location.href;
-const id = url.slice(32);
+const id = url.slice(44);
